7a02bf3ca1067a667043e0e002fe038cd80bdf4e
smritibhawana <bhawanasmriti@gmail.com>
2025-03-22 03:44:55 +0530

    Updated filename with changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-14 text-center">
@@ -5,13 +6,16 @@ const Footer = () => {
       <p className="text-gray-400 mt-2">"We all can work; but together we win"</p>
       <p className="text-gray-400 mb-4">- Smriti Bhawana</p>
       
-      <div className="flex flex-col md:flex-row items-center justify-center gap-6 text-gray-300 text-sm">
-        
+      <div className="flex flex-col md:flex-row items-center justify-center gap-6 text-gray-300 text-sm pt-5">
+      
+      <a href="https://github.com/SmritiBhawana" className="flex items-center gap-2 hover:text-gray-100">
+      <FaGithub className="text-white text-2xl hover:text-gray-400" /> github.com/SmritiBhawana
+        </a>
         <a href="mailto:engsmritibhawana@gmail.com" className="flex items-center gap-2 hover:text-gray-100">
-          <i className="fas fa-envelope"></i> engsmritibhawana@gmail.com
+        <FaEnvelope className="text-white text-2xl hover:text-gray-400" /> engsmritibhawana@gmail.com
         </a>
         <a href="https://www.linkedin.com/in/smriti-bhawana" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:text-gray-100">
-          <i className="fab fa-linkedin"></i> linkedin.com/in/smriti-bhawana
+        <FaLinkedin className="text-white text-2xl hover:text-gray-400" /> linkedin.com/in/smriti-bhawana
         </a>
       </div>
     </footer>
