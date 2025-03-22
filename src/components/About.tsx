9c0967813930e9746a3543341793c1e99306a228
smritibhawana <bhawanasmriti@gmail.com>
2025-03-22 21:16:40 +0530

    Updated filename with changes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,18 +1,21 @@
 const About: React.FC = () => {
-    return (
-      <section className="py-14 flex flex-col items-center justify-center px-6 md:px-20">
-      <h2 className="text-4xl font-bold text-teal-600 mb-2">About Me</h2>
-      <p className="text-lg  mb-10">
-        I'm a passionate Front-End Developer who loves crafting beautiful and
-        interactive UIs.
-      </p>
+  return (
+    <section className="py-14 flex flex-col items-center justify-center px-6 md:px-20">
+      <h2 className="text-4xl font-bold text-teal-600 mb-2">Profile</h2>
+      <p className="text-lg  mb-10">I'm a creative Web Developer</p>
 
       <div className="flex flex-col md:flex-row items-center max-w-5xl w-full">
         {/* Left Section - About Me Text */}
         <div className="md:w-1/3 text-center md:text-left">
           <h3 className="text-2xl font-semibold text-teal-600">About Me</h3>
           <p className="mt-4">
-          I am a creative software engineer, diligence and passion to learn and work on new technologies are two traits that I believe most reflect me as an individual. I always try to achieve a higher level of perfection in my work. I believe true wisdom lies in gathering the precious things out of each day as it goes by. I like working in a team, you will learn faster and much more. As the saying goes: 'two heads are better than one.'
+            I am a creative software engineer, diligence and passion to learn
+            and work on new technologies are two traits that I believe most
+            reflect me as an individual. I always try to achieve a higher level
+            of perfection in my work. I believe true wisdom lies in gathering
+            the precious things out of each day as it goes by. I like working in
+            a team, you will learn faster and much more. As the saying goes:
+            'two heads are better than one.'
           </p>
         </div>
 
@@ -34,17 +37,19 @@ const About: React.FC = () => {
           <p className="mt-2">
             <strong>Location:</strong> Varanasi
           </p>
-          <p className="mt-10"> <a href="/front_end.pdf" download className="bg-teal-600 text-white px-4 py-2 rounded-md hover:bg-teal-700 ">
-  Download Resume
-</a></p>
-         
+          <p className="mt-10">
+            {" "}
+            <a
+              href="/front_end.pdf"
+              download
+              className="bg-teal-600 text-white px-4 py-2 rounded-md hover:bg-teal-700 "
+            >
+              Download Resume
+            </a>
+          </p>
         </div>
       </div>
-
-      
-
     </section>
-    );
-  };
-  export default About;
-  
+  );
+};
+export default About;
