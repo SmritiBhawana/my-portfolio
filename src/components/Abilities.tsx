53320b21f498a6ff980fa11709745115406b82c5
smritibhawana <bhawanasmriti@gmail.com>
2025-03-22 14:36:20 +0530

    Updated filename with changes

--- src/components/Abilities.tsx
+++ src/components/Abilities.tsx
@@ -1,56 +1,39 @@
 const Abilities = () => {
   const abilities = [
     { name: "Javascript", rating: 4 },
+    { name: "TypeScript", rating: 4 },
     { name: "React", rating: 3 },
-    { name: "Angular", rating: 2 },
-    { name: "ES6", rating: 3 },
-    { name: "JSON", rating: 3 },
-    { name: "Bootstrap Framework", rating: 4 },
-    { name: "CSS", rating: 3 },
-    { name: "HTML(5)", rating: 2 },
-    { name: "C#", rating: 2 },
-    { name: "Command line Interface", rating: 2 },
-    { name: "Node JS", rating: 3 }
-  ];
 
-  const abilities2 = [
+    { name: "Tailwind CSS", rating: 4 },
+    { name: "CSS", rating: 3 },
+    { name: "HTML", rating: 4 },
+    { name: "Bootstrap Framework", rating: 4 },
+    { name: "Node JS", rating: 3 },
     { name: "Git/Git Flow", rating: 4 },
-    { name: "Redux", rating: 3 },
     { name: "Object Oriented Programming", rating: 4 },
-    { name: "MVC Pattern", rating: 3 },
-    { name: "Singleton Pattern", rating: 3 },
-    { name: "Performance Optimization", rating: 4 },
-    { name: "MySQL", rating: 3 },
-    { name: "Mongo DB", rating: 2 },
-    { name: "Azure DevOps", rating: 2 },
-    { name: "Command line Interface", rating: 2 },
-    { name: "Material Design", rating: 3 },
+    { name: "Web Performance Optimization", rating: 4 },
   ];
 
   return (
     <div className="bg-[#F9F8E3] p-14">
-      <h2 className="text-3xl font-bold text-center text-teal-700">Abilities</h2>
+      <h2 className="text-3xl font-bold text-center text-teal-700">
+        Abilities
+      </h2>
       <p className="text-center text-gray-600 italic mb-10">
         "Knowledge is the power of the wise man." - William Pen
       </p>
-      <div className="grid grid-cols-2 max-w-4xl mx-auto gap-10">
-        <div>
+
+      <div className="flex flex-wrap justify-center gap-4">
+        <div className="sm:w-1/2 w-full">
           <h3 className="text-xl font-bold text-teal-700">Skills</h3>
-          <ul className="mt-4 space-y-2">
+          <ul className="mt-4">
             {abilities.map((ability, index) => (
               <li key={index} className="flex justify-between text-gray-700">
                 {ability.name}
-                <span className="text-teal-500">{"★".repeat(ability.rating)}{"☆".repeat(5 - ability.rating)}</span>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div>
-          <ul className="mt-4 space-y-2 pt-7">
-            {abilities2.map((ability, index) => (
-              <li key={index} className="flex justify-between text-gray-700">
-                {ability.name}
-                <span className="text-teal-500">{"★".repeat(ability.rating)}{"☆".repeat(5 - ability.rating)}</span>
+                <span className="text-teal-500">
+                  {"★".repeat(ability.rating)}
+                  {"☆".repeat(5 - ability.rating)}
+                </span>
               </li>
             ))}
           </ul>
